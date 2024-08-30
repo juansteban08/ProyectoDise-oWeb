f47e01d5347f1e424225ae770dbabb0406658c92
juansteban08 <juan_steban.valverde@uao.edu.co>
2024-08-29 19:52:46 -0500

    mejora de resolucion

--- fitness-bites/server.js
+++ fitness-bites/server.js
@@ -6,11 +6,11 @@ const cors = require('cors');
 const app = express();
 const PORT = 5000;
 
-// Middleware
+
 app.use(bodyParser.json());
 app.use(cors());
 
-// Conectar a MongoDB
+
 mongoose.connect('mongodb+srv://juanstebanvalverde:AIcnluNZrG7bATQJ@cluster0.dohfa.mongodb.net/ProyectoDiseñoWeb?retryWrites=true&w=majority')
     .then(() => {
         console.log('Conectado a MongoDB');
@@ -19,17 +19,17 @@ mongoose.connect('mongodb+srv://juanstebanvalverde:AIcnluNZrG7bATQJ@cluster0.doh
         console.error('Error al conectar a MongoDB', err);
     });
 
-// Definir un esquema de datos para los contactos
+
 const contactoSchema = new mongoose.Schema({
     nombre: { type: String, required: true },
     correo: { type: String, required: true, unique: true },
     mensaje: { type: String, required: true }
 });
 
-// Crear un modelo basado en el esquema
+
 const Contacto = mongoose.model('Contacto', contactoSchema);
 
-// Ruta para manejar las solicitudes del formulario de contacto
+
 app.post('/api/contacto', (req, res) => {
     const nuevoContacto = new Contacto({
         nombre: req.body.nombre,
@@ -42,7 +42,7 @@ app.post('/api/contacto', (req, res) => {
         .catch(err => res.status(400).json({ error: err.message }));
 });
 
-// Iniciar el servidor
+
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
